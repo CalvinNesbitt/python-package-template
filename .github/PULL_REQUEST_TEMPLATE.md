ef504a40d67861eeb99ae650b04f9a899c121cbb
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2024-07-20 13:29:45 +0100

    feat: updated template

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,7 @@
+### Description
+Please explain the changes you made here.
+
+### Checklist
+- [ ] Dependencies install correctly in a clean environment and code executes;
+- [ ] Test coverage has been extended (if necessary);
+- [ ] README, documentation and/or docstrings have been expanded as necessary.
--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name="package_name",
-    version="0.1",
-    description="This is a package.",
-    author="Calvin Nesbitt",
-    author_email="cf.nesbitt94@gmail.com",
-    license="MIT",
-    packages=find_packages(),
-    zip_safe=False,
-    install_requires=requirements,
-)
+setup()
